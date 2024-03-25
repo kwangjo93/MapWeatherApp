f4ecf8d36a1b2e9a3dff946b600d7a6f1df434c0
kwangjo93 <125628009+kwangjo93@users.noreply.github.com>
2024-03-25 10:18:51 +0900

    Initial Commit

---
+++ MapWeatherApp/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  MapWeatherApp
+//
+//  Created by 천광조 on 3/25/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
